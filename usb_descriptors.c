b46c1f599f21b4ec257008148e9adcfce093e0da
sctanf <36978460+sctanf@users.noreply.github.com>
2025-08-01 01:20:52 -0500

    change usb_descriptors to pico amp 2

--- usb_descriptors.c
+++ usb_descriptors.c
@@ -149,11 +149,11 @@ enum {
 char const *string_desc_arr[] =
 {
   (const char[]) { 0x09, 0x04 },  // 0: is supported language is English (0x0409)
-  "TinyUSB",                     // 1: Manufacturer
-  "TinyUSB headset",                     // 2: Product
+  "sctanf",                     // 1: Manufacturer
+  "Pico Amp 2",                     // 2: Product
   NULL,                           // 3: Serials will use unique ID if possible
-  "TinyUSB Speakers",            // 4: Audio Interface
-  "TinyUSB Microphone",          // 5: Audio Interface
+  "Pico Amp 2",            // 4: Audio Interface
+  "Pico Amp 2",          // 5: Audio Interface
 };
 
 static uint16_t _desc_str[32 + 1];
